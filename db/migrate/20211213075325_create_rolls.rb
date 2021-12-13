98f720d87f6d35eef0c7f1b840c4644760752b42
Airene Maria <aireneapacible@gmail.com>
2021-12-13 16:00:42 +0800

    Creates rating, roll, and entry databases

---
+++ db/migrate/20211213075325_create_rolls.rb
@@ -0,0 +1,12 @@
+class CreateRolls < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rolls do |t|
+      t.string :title
+      t.date :start_date
+      t.date :end_date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_12_13_080025) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.date "date"
+    t.string "country"
+    t.string "city"
+    t.boolean "public"
+    t.string "category"
+    t.string "category_response"
+    t.text "description"
+    t.bigint "roll_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["roll_id"], name: "index_entries_on_roll_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.string "country"
+    t.decimal "rating"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rolls", force: :cascade do |t|
+    t.string "title"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_rolls_on_user_id"
+  end
+
+end
