362f3058ec4ed4587508ac1f22853b694c94af36
bananakiu <leandrenoelkiu@gmail.com>
2022-01-04 18:49:30 +0800

    delete previous implementation of user authentication (with devise)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations'
-             }
-  get '/member-data', to: 'members#show'
   namespace :api do
     namespace :v1 do
       resources :rolls
