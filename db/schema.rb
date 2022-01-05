4f38067d9c80e257d7fdb43407df20e37a0abaa1
bananakiu <leandrenoelkiu@gmail.com>
2022-01-05 18:21:56 +0800

    setup user model and controlle

--- db/schema.rb
+++ db/schema.rb
@@ -31,12 +31,6 @@ ActiveRecord::Schema.define(version: 2021_12_13_080025) do
     t.index ["roll_id"], name: "index_entries_on_roll_id"
   end
 
-  create_table "jwt_denylist", force: :cascade do |t|
-    t.string "jti", null: false
-    t.datetime "exp", null: false
-    t.index ["jti"], name: "index_jwt_denylist_on_jti"
-  end
-
   create_table "ratings", force: :cascade do |t|
     t.string "country"
     t.decimal "rating"
@@ -56,15 +50,10 @@ ActiveRecord::Schema.define(version: 2021_12_13_080025) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "entries", "rolls"
