eab29655b242a5ab6bb118d0ea938733911a44a2
Airene Maria <aireneapacible@gmail.com>
2022-01-11 23:58:43 +0800

    Adds rolls in user endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :rolls
+      get '/rolls/user/:id', :to => 'rolls#rolls_in_user', as: 'rolls_in_user'
       resources :entries
       get '/entries/roll/:id', :to => 'entries#entries_in_roll', as: 'entries_in_roll'
       get '/entries/country/:country', :to => 'entries#entries_in_country', as: 'entries_in_country'
