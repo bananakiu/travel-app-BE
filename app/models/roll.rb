d0eebd9ea9783e71dab5f965c6253d40c9b97c67
Airene Maria <aireneapacible@gmail.com>
2022-01-15 11:46:54 +0800

    Remove validation causing problems

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,14 +1,5 @@
-class MyValidator < ActiveModel::Validator
-  def end_date_after_start_date?
-    if end_date < start_date
-      errors.add :end_date, "must be after start date"
-    end
-  end
-  end  
-
 class Roll < ApplicationRecord
   belongs_to :user
   has_many :entries
   accepts_nested_attributes_for :entries, allow_destroy: true
-  validates_with MyValidator
 end
